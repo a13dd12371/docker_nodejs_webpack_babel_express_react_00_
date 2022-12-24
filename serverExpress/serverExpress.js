6466de2c9e6ff550fef51e0971d41c2095b35793
a13dd12371 <108872467+a13dd12371@users.noreply.github.com>
2022-12-25 00:42:24 +0300

    serverExpress.js

---
+++ serverExpress/serverExpress.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const path = require('path');
+
+const _Port = process.env.PORT || 8080
+
+const app = express()
+const router = require('express').Router()
+
+app.use(
+  '/',
+  router.get('/', (request, response) => {
+    console.log(`Request Method: ${request.method}`)
+    console.log(`Request Route: ${url.parse(request.url).pathname}`)
+    console.log(`Request Port: ${_Port}`)
+    response.sendFile(path.join(__dirname, '..', '/clientHTML', '/index.html'))
+  })
+)
+app.use(
+  '/xxx',
+  router.get('/xxx', (request, response) => {
+    response.send("<h1>Hello, Express! - xxx</h1>")
+  })
+)
+
+app.listen(
+  _Port,
+  console.log(
+    `\t ** serverHttp/serverHttp.js >> requestListerner
+ __dirname: ${__dirname})
+ process.cwd(): ${process.cwd()}`
+  )
+)
